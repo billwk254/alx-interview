b1ee8407bb6afe832453b48e5425a3d3122cbda9
billwk254 <billkihungu51@gmail.com>
2024-02-15 15:14:48 +0300

    Semistandard fix applied

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,28 +1,37 @@
 #!/usr/bin/node
 
+// Import the 'request' module
 const request = require('request');
 
-const movieId = process.argv[2];
-const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+// Send a request to the Star Wars API to fetch information about the film with the specified ID
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  // Handle errors
+  if (err) throw err;
 
-request(apiUrl, function (error, response, body) {
-  if (error) {
-    console.error('Error:', error);
-    return;
-  }
+  // Parse the response body to extract the list of actors in the film
+  const actors = JSON.parse(body).characters;
 
-  const filmData = JSON.parse(body);
-  const characters = filmData.characters;
-
-  characters.forEach((characterUrl) => {
-    request(characterUrl, function (error, response, body) {
-      if (error) {
-        console.error('Error:', error);
-        return;
-      }
-
-      const characterData = JSON.parse(body);
-      console.log(characterData.name);
-    });
-  });
+  // Call the function to print the names of the actors in the exact order they appear in the film
+  printActorsInOrder(actors, 0);
 });
+
+// Function to print the names of the actors in the exact order they appear in the film
+const printActorsInOrder = (actors, index) => {
+  // Base case: If all actors have been printed, return
+  if (index === actors.length) return;
+
+  // Send a request to fetch information about the actor at the current index
+  request(actors[index], function (err, res, body) {
+    // Handle errors
+    if (err) throw err;
+
+    // Parse the response body to extract the name of the actor
+    const actorName = JSON.parse(body).name;
+
+    // Print the name of the actor
+    console.log(actorName);
+
+    // Recursively call the function to print the next actor in the list
+    printActorsInOrder(actors, index + 1);
+  });
+};
